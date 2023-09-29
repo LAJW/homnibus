6d3eaeb5a8acc98800483e610b7f45b6da992c6f
Łukasz Wrona <lukasz.andrzej.wrona@outlook.com>
2023-09-29 01:47:21 +0100

    Add error handling around Jira Connecting/Parsing

--- JiraExtractor/Program.fs
+++ JiraExtractor/Program.fs
@@ -1,14 +1,43 @@
-﻿open System.Net.Http
+﻿open System.Net
+open System.Net.Http
 open System.Text.Json
 open FSharpPlus
 open System
+open FSharpPlus.Control
 open Homnibus.Core.Jira
+open System.Threading.Tasks
 
 let searchUrl (maxResults: int) (searchUrl: string) : string =
     searchUrl + $"&expand=changelog&maxResults={maxResults}"
 
 let surround (ch : char) (str : string) = $"{ch}{str}{ch}"
 let join (delim : string) (strings : string seq) = String.Join(delim, strings)
+
+type AuthorizedClient(token : string) =
+    let client = new HttpClient()
+    interface IDisposable with
+        member _.Dispose() = client.Dispose()
+
+    member _.GetJSON<'T>(url : string) : Task<Result<'T, string>> =
+        task {
+            let request = new HttpRequestMessage(HttpMethod.Get, url)
+            request.Headers.Add("Authorization", $"Bearer {token}")
+            try
+                let! response = client.SendAsync(request)
+                match response.StatusCode with
+                | HttpStatusCode.OK ->
+                    let! body = response.Content.ReadAsStringAsync()
+                    return Ok(JsonSerializer.Deserialize<'T> body)
+                | HttpStatusCode.Unauthorized -> return Error "Bad token"
+                | HttpStatusCode.Forbidden -> return Error "Can't access resource"
+                | HttpStatusCode.NotFound -> return Error "Not Found"
+                | status -> return Error $"Unexpected status: {status}"
+            with
+                | :? HttpRequestException -> return Error "Could not connect"
+                | :? JsonException -> return Error "Bad JSON received"
+        }
+        
+type X = { k : Choice }
 
 [<EntryPoint>]
 let main (args : string array) =
@@ -18,19 +47,11 @@ let main (args : string array) =
         let filterUrl (filterID: string) = $"{jiraHostUrl}/rest/api/2/filter/{filterID}"
         let url = filterUrl(filterId)
 
-        task {
-            use client = new HttpClient()
-            let fetch (url : string) =
-                task {
-                    let request = new HttpRequestMessage(HttpMethod.Get, url)
-                    request.Headers.Add("Authorization", $"Bearer {token}") // TODO: Error handling bad auth / 404
-                    let! response = client.SendAsync(request)
-                    return! response.Content.ReadAsStringAsync()
-                }
-            let! body = fetch url
-            let filter = JsonSerializer.Deserialize<FilterModel> body // TODO: Can be null
-            let! body = filter.searchUrl |> searchUrl maxResults |> fetch
-            let search = JsonSerializer.Deserialize<SearchModel> body
+        use client = new AuthorizedClient(token)
+        client.GetJSON<FilterModel>(url)
+        |> Task.bind(Result.bimap Task.FromResult (fun filter ->
+            filter.searchUrl |> searchUrl maxResults |> client.GetJSON<SearchModel>)
+        |> Task.map(Result.map(fun search ->
             // "Ticket No","Status","Days In Column","Ticket Type","Priority","Components","Epic Key","Summary","Date","Flagged","Label","Story Points","Created Date","Previous Statuses"
             let header = """Ticket No","Status","Days In Column","Ticket Type","Priority","Components","Epic Key","Summary","Date","Flagged","Label","Story Points","Created Date","Previous Statuses"""
             printfn $"\"%s{header}\""
@@ -52,10 +73,14 @@ let main (args : string array) =
                 let statuses = issue.Statuses |> List.collect (fun status -> [status.Date.ToString(dateOutFormat); status.Name])
                 let row = [ticketNo; status; daysInCC; ticketType; priority; component_; epicKey; summary; date; flagged; label; storyPoints; createdDate] @ statuses
                 row |> map (surround '"') |> join "," |> printfn "%s"
-        }
+        ))
         |> Async.AwaitTask
         |> Async.RunSynchronously
-        0
+        |> function
+            | Ok _ -> 0
+            | Error ex ->
+                printfn "%s" ex
+                1
     | _ ->
         printfn "Bad arguments, use ./JiraExtractor.exe <Jira host URL> <jira token> <filter id>"
         1
